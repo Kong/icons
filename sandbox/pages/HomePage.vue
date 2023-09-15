1d3bfd6d574078a8c8e4e4621fad274042426e53
Adam DeHaven <adam.dehaven@konghq.com>
2023-09-15 10:15:19 -0400

    chore: sandbox icon sections

--- sandbox/pages/HomePage.vue
+++ sandbox/pages/HomePage.vue
@@ -2,15 +2,49 @@
   <PageHeader @search="(query: string) => searchQuery = query" />
   <div class="sandbox-layout">
     <div class="sandbox-container">
-      <div
-        v-if="filteredComponents.length"
-        class="icon-container"
-      >
-        <SandboxIcon
-          v-for="(icon, idx) in filteredComponents"
-          :key="`icon-${idx}`"
-          :icon="icon.component"
-        />
+      <div v-if="filteredComponents.length">
+        <!-- Solid Icons -->
+        <div
+          v-if="filteredComponents.filter((icon) => icon.type === 'solid').length"
+          class="icon-container"
+        >
+          <h2>Solid Icons</h2>
+          <div class="icon-grid">
+            <SandboxIcon
+              v-for="(icon, idx) in filteredComponents.filter((icon) => icon.type === 'solid')"
+              :key="`icon-${idx}`"
+              :icon="icon.component"
+            />
+          </div>
+        </div>
+        <!-- Multi-Color Icons -->
+        <div
+          v-if="filteredComponents.filter((icon) => icon.type === 'multi-color').length"
+          class="icon-container"
+        >
+          <h2>Multi-Color Icons</h2>
+          <div class="icon-grid">
+            <SandboxIcon
+              v-for="(icon, idx) in filteredComponents.filter((icon) => icon.type === 'multi-color')"
+              :key="`icon-${idx}`"
+              :icon="icon.component"
+            />
+          </div>
+        </div>
+        <!-- Flag Icons -->
+        <div
+          v-if="filteredComponents.filter((icon) => icon.type === 'flags').length"
+          class="icon-container"
+        >
+          <h2>Flag Icons</h2>
+          <div class="icon-grid">
+            <SandboxIcon
+              v-for="(icon, idx) in filteredComponents.filter((icon) => icon.type === 'flags')"
+              :key="`icon-${idx}`"
+              :icon="icon.component"
+            />
+          </div>
+        </div>
       </div>
       <p v-else>
         No icons match your query. Try searching again.
@@ -23,14 +57,37 @@
 import { ref, computed } from 'vue'
 import PageHeader from '../components/PageHeader.vue'
 import SandboxIcon from '../components/SandboxIcon.vue'
-import * as iconComponents from '../../src/components'
+import * as solidIcons from '../../src/components/solid'
+import * as multiColorIcons from '../../src/components/multi-color'
+import * as flagIcons from '../../src/components/flags'
 
 const searchQuery = ref('')
+
 const filteredComponents = computed(() => {
   const allComponents = []
 
-  for (const [key, val] of Object.entries(iconComponents)) {
+  // solid icons
+  for (const [key, val] of Object.entries(solidIcons)) {
     allComponents.push({
+      type: 'solid',
+      name: key,
+      component: val,
+    })
+  }
+
+  // multi-color icons
+  for (const [key, val] of Object.entries(multiColorIcons)) {
+    allComponents.push({
+      type: 'multi-color',
+      name: key,
+      component: val,
+    })
+  }
+
+  // flags
+  for (const [key, val] of Object.entries(flagIcons)) {
+    allComponents.push({
+      type: 'flags',
       name: key,
       component: val,
     })
@@ -64,11 +121,26 @@ $content-max-width: 1800px;
 }
 
 .icon-container {
+  border-bottom: $kui-border-width-10 solid $kui-color-border;
+  margin-bottom: $kui-space-70;
+
+  &:first-of-type {
+    padding-top: $kui-space-0;
+  }
+
+  &:last-of-type {
+    border-bottom: none;
+    margin-bottom: $kui-space-0;
+  }
+}
+
+.icon-grid {
   display: grid;
   gap: $kui-space-50;
   grid-template-columns: repeat(2, minmax(0, 1fr));
-  margin: 0 auto;
+  margin: 0 auto $kui-space-50;
   max-width: $content-max-width;
+  padding-bottom: $kui-space-70;
   width: 100%;
 
   @media (min-width: $kui-breakpoint-mobile) {
@@ -86,6 +158,11 @@ $content-max-width: 1800px;
   @media (min-width: $kui-breakpoint-desktop) {
     grid-template-columns: repeat(6, minmax(0, 1fr));
   }
+}
+
+h2 {
+  color: $kui-color-text;
+  margin-top: $kui-space-0;
 }
 
 p {
